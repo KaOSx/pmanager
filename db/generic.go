941f5f2ace31445e5f8f0cba99144d1c8246ca88
Benjamin VAUDOUR <b.vaudour@luence.fr>
2021-02-20 13:23:56 +0100

    Correct pagination issue (2)

--- db/generic.go
+++ db/generic.go
@@ -265,7 +265,7 @@ func (dl Datalist) Slice(limit, offset int64) Datalist {
 	if offset >= 0 {
 		dl = dl[offset:]
 	}
-	if limit < len(dl) {
+	if limit < int64(len(dl)) {
 		dl = dl[:limit]
 	}
 	return dl
