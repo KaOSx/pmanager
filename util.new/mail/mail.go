69c4067f544af56f6bdfe183bd3ebfbc899c5c81
Benjamin VAUDOUR <b.vaudour@luence.fr>
2021-08-17 22:22:58 +0200

    Implement mailer package

---
+++ util.new/mail/mail.go
@@ -0,0 +1,82 @@
+package mail
+
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
+type header struct {
+	key   string
+	value string
+}
+
+func (h header) String() string {
+	return h.key + ": " + h.value
+}
+
+type Mail struct {
+	from    string
+	to      []string
+	headers []header
+	subject string
+	body    string
+}
+
+func (m *Mail) From(from string) *Mail {
+	m.from = from
+	return m
+}
+
+func (m *Mail) To(to ...string) *Mail {
+	m.to = append(m.to, to...)
+	return m
+}
+
+func (m *Mail) Header(key, value string) *Mail {
+	m.headers = append(m.headers, header{
+		key:   key,
+		value: value,
+	})
+	return m
+}
+
+func (m *Mail) Subject(subject string) *Mail {
+	m.subject = subject
+	return m
+}
+
+func (m *Mail) Body(body string) *Mail {
+	m.body = body
+	return m
+}
+
+func (m Mail) contains(key string) bool {
+	for _, h := range m.headers {
+		if h.key == key {
+			return true
+		}
+	}
+	return false
+}
+
+func (m Mail) Reader() io.Reader {
+	var buf bytes.Buffer
+	if !m.contains("From") {
+		buf.WriteString("From: " + m.from + "\r\n")
+	}
+	if !m.contains("To") {
+		buf.WriteString("To: " + strings.Join(m.to, ";") + "\r\n")
+	}
+	for _, h := range m.headers {
+		buf.WriteString(h.String() + "\r\n")
+	}
+	if !m.contains("Subject") {
+		buf.WriteString("Subject: " + m.subject + "\r\n")
+	}
+	if !m.contains("Content-Type") {
+		buf.WriteString("Content-Type: text/plain; charset=\"utf-8\"\r\n")
+	}
+	buf.WriteString("\r\n" + m.body + "\r\n")
+	return &buf
+}
---
+++ util.new/mail/server.go
@@ -0,0 +1,90 @@
+package mail
+
+import (
+	"bufio"
+	"crypto/tls"
+	"net"
+	"net/smtp"
+)
+
+var (
+	srv server
+)
+
+type server struct {
+	host     string
+	port     string
+	tls      bool
+	user     string
+	password string
+}
+
+func (s server) name() string {
+	return s.host + ":" + s.port
+}
+
+func (s server) auth() smtp.Auth {
+	return smtp.PlainAuth("", s.user, s.password, s.host)
+}
+
+func (s server) dial() (net.Conn, error) {
+	if s.tls {
+		tlsconfig := &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         s.host,
+		}
+		return tls.Dial("tcp", s.name(), tlsconfig)
+	}
+	return net.Dial("tcp", s.name())
+}
+
+func (s server) client() (*smtp.Client, error) {
+	dial, err := s.dial()
+	if err != nil {
+		return nil, err
+	}
+	c, err := smtp.NewClient(dial, s.host)
+	if err == nil {
+		err = c.Auth(s.auth())
+	}
+	return c, err
+}
+
+func (s server) send(m Mail) error {
+	c, err := s.client()
+	if err != nil {
+		return err
+	}
+	if err := c.Mail(m.from); err != nil {
+		return err
+	}
+	for _, t := range m.to {
+		if err := c.Rcpt(t); err != nil {
+			return err
+		}
+	}
+	wc, err := c.Data()
+	if err != nil {
+		return err
+	}
+	defer wc.Close()
+	_, err = bufio.NewReader(m.Reader()).WriteTo(wc)
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
+
+func InitSmtp(host, port, user, password string, tls bool) {
+	srv = server{
+		host:     host,
+		port:     port,
+		user:     user,
+		password: password,
+		tls:      tls,
+	}
+}
+
+func Send(mail Mail) error {
+	return srv.send(mail)
+}
