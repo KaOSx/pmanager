7361aafe03f99902a51708c04f0914d424f41054
Benjamin VAUDOUR <b.vaudour@luence.fr>
2021-08-18 12:00:52 +0200

    json conversions

---
+++ util.new/conv/json.go
@@ -0,0 +1,33 @@
+package conv
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+)
+
+func ReadJson(r io.Reader, v interface{}) error {
+	d := json.NewDecoder(r)
+	return d.Decode(v)
+}
+
+func WriteJson(w io.Writer, v interface{}, beautify bool) error {
+	e := json.NewEncoder(w)
+	if beautify {
+		e.SetIndent("", "    ")
+	}
+	return e.Encode(v)
+}
+
+func ToJson(v interface{}, beautify bool) []byte {
+	var w bytes.Buffer
+	WriteJson(&w, v, beautify)
+	return w.Bytes()
+}
+
+func ToMap(v interface{}) Map {
+	m := make(Map)
+	b, _ := json.Marshal(v)
+	json.Unmarshal(b, &m)
+	return m
+}
